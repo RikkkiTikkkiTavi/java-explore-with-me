d6f240286f759e7d83cae28cfaba94bc32b36d5f
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-09 18:45:28 +0300

    feet:add errorHandler

---
+++ stats-service/server/src/main/java/ru/practicum/explorewithme/hit/handler/ErrorResponse.java
@@ -0,0 +1,10 @@
+package ru.practicum.explorewithme.hit.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final String error;
+}
