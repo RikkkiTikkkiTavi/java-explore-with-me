5e05bda0931715b47ab611d3e3339b885d356eeb
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-11 02:21:41 +0300

    feat:add exception for validator

---
+++ stats-service/server/src/main/java/ru/practicum/server/hit/validator/HitValidationException.java
@@ -0,0 +1,7 @@
+package ru.practicum.server.hit.validator;
+
+public class HitValidationException extends RuntimeException {
+    public HitValidationException(String message) {
+        super(message);
+    }
+}
