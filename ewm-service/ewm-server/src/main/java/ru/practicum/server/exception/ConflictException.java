9af4885d8f854fa0e37edd2a3f52fe03816875f2
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 01:35:50 +0300

    feat: add exception

---
+++ ewm-service/ewm-server/src/main/java/ru/practicum/server/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package ru.practicum.server.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
