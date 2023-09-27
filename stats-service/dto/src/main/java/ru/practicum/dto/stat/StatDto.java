3aef70795311198f7991961842aea8dd2e99b4f8
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 01:39:03 +0300

    style: rename class

--- stats-service/dto/src/main/java/ru/practicum/dto/stat/StatDto.java
+++ stats-service/dto/src/main/java/ru/practicum/dto/stat/StatDto.java
@@ -1,11 +1,11 @@
-package ru.practicum.dto.state;
+package ru.practicum.dto.stat;
 
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
-public class StateDto {
+public class StatDto {
     String app;
     String uri;
     int hits;
