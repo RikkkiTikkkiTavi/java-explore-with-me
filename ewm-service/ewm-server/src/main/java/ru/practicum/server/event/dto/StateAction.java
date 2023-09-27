517da5dc14daa22b34e28d59a6fd81111d8f5075
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 01:36:16 +0300

    feat: add status enum

---
+++ ewm-service/ewm-server/src/main/java/ru/practicum/server/event/dto/StateAction.java
@@ -0,0 +1,5 @@
+package ru.practicum.server.event.dto;
+
+public enum StateAction {
+    PUBLISH_EVENT, REJECT_EVENT, SEND_TO_REVIEW, CANCEL_REVIEW
+}
---
+++ ewm-service/ewm-server/src/main/java/ru/practicum/server/request/model/Status.java
@@ -0,0 +1,5 @@
+package ru.practicum.server.request.model;
+
+public enum Status {
+    CONFIRMED, REJECTED, PENDING, CANCELED
+}
