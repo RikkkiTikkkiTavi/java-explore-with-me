b2d9e648407ec85cf65a60e0a3e1ce1bf9ca137a
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 01:36:46 +0300

    refactor: add @import

--- ewm-service/ewm-server/src/main/java/ru/practicum/server/ExploreWithMeMainServer.java
+++ ewm-service/ewm-server/src/main/java/ru/practicum/server/ExploreWithMeMainServer.java
@@ -2,7 +2,10 @@ package ru.practicum.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.practicum.client.StatsClient;
 
+@Import({StatsClient.class})
 @SpringBootApplication
 public class ExploreWithMeMainServer {
     public static void main(String[] args) {
