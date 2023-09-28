3dba39224306901d116daf1e6a8fbdbdf8fd04b2
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 14:01:43 +0300

    refactor: add new response status

--- stats-service/server/src/main/java/ru/practicum/server/hit/HitController.java
+++ stats-service/server/src/main/java/ru/practicum/server/hit/HitController.java
@@ -2,6 +2,7 @@ package ru.practicum.server.hit;
 
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.hit.HitDto;
 import ru.practicum.dto.stat.StatDto;
@@ -18,15 +19,16 @@ public class HitController {
     private HitService service;
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public void add(@RequestBody HitDto hitDto) {
         service.add(hitDto);
     }
 
     @GetMapping("/stats")
     public List<StatDto> getState(@RequestParam(name = "start")
-                                   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                   @RequestParam(name = "end")
-                                   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                   @RequestParam(required = false) List<String> uris,
                                   @RequestParam(defaultValue = "false") boolean unique) {
         return service.getState(start, end, uris, unique);
