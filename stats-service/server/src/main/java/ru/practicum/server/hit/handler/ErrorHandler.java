85b20cb9dd1dda89422155f4c2239649e82212f7
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-28 14:01:00 +0300

    feat: add new handler

--- stats-service/server/src/main/java/ru/practicum/server/hit/handler/ErrorHandler.java
+++ stats-service/server/src/main/java/ru/practicum/server/hit/handler/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.server.hit.handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,13 +13,18 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidation(final HitValidationException e) {
-        return new ErrorResponse(
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConflict(final MissingServletRequestParameterException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
