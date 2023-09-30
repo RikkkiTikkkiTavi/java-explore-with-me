cbc944c9179c8c9007b725bee67ea0c42fdb79ee
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-09-30 11:29:28 +0300

    refactor:changed method signature

--- ewm-service/src/main/java/ru/practicum/server/event/service/EventService.java
+++ ewm-service/src/main/java/ru/practicum/server/event/service/EventService.java
@@ -1,11 +1,11 @@
 package ru.practicum.server.event.service;
 
 import ru.practicum.server.event.dto.*;
-import ru.practicum.server.event.model.EventState;
+import ru.practicum.server.event.repository.AdminEventsParameters;
+import ru.practicum.server.event.repository.PublicEventsParameters;
 import ru.practicum.server.request.dto.ParticipationRequestDto;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventService {
@@ -22,14 +22,11 @@ public interface EventService {
     EventRequestStatusUpdateResult updateEventRequestStatus(int userId, int eventId,
                                                             EventRequestStatusUpdateRequest request);
 
-    List<EventFullDto> getFilteredEvents(List<Integer> users, List<EventState> states, List<Integer> categories,
-                                         LocalDateTime rangeStart, LocalDateTime rangeEnd, int from, int size);
+    List<EventFullDto> getFilteredEvents(AdminEventsParameters param, int from, int size);
 
     EventFullDto updateByAdmin(int eventId, UpdateEventAdminRequest updateEventAdminRequest);
 
-    List<EventShortDto> getPublicEvents(String text, List<Integer> category, Boolean paid, Boolean onlyAvailable,
-                                        String sort, LocalDateTime rangeStart, LocalDateTime rangeEnd, int from,
-                                        int size, HttpServletRequest request);
+    List<EventShortDto> getPublicEvents(PublicEventsParameters param, int from, int size, HttpServletRequest request);
 
     EventFullDto getEventInfo(int eventId, HttpServletRequest request);
 }
